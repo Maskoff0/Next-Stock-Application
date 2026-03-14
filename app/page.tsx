9a0e584049cade0c47bff9b29374b771229d4ae4
Maskoff0 <162310729+Maskoff0@users.noreply.github.com>
2026-03-14 13:07:02 +0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex justify-center h-screen items-center'>
+      <Button>Click Me!</Button>
+    </div>
   )
 }
 
